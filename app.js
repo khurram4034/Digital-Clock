b457bfeaa0f59e465fbd3e25739c0187b9be086b
khurram4034 <khurram2201022@gmail.com>
2020-08-20 19:09:22 +0500

    Digital Clock code updated

---
+++ app.js
@@ -0,0 +1,27 @@
+function realtimeClock(){
+    var rtClock = new Date();
+    var hours = rtClock.getHours();
+    var minutes = rtClock.getMinutes();
+    var seconds = rtClock.getSeconds();
+
+    //Add AM and PM system
+
+    var amPm = (hours < 12) ? "AM" : "PM";
+
+    //Convert the hours Componet to 12-hour format
+
+    hours = (hours >12) ? hours - 12 : hours;
+
+    //Pad the hours,minutes and seconds with leading zeros
+
+    hours = ("0" + hours).slice(-2);
+    minutes = ("0" + minutes).slice(-2);
+    seconds = ("0" + seconds).slice(-2);
+
+    //Display the Clock
+
+    document.getElementById('clock').innerHTML = hours + " : " + minutes + " : " + seconds + " " + amPm;
+
+    var t = setTimeout(realtimeClock, 500);
+
+}
